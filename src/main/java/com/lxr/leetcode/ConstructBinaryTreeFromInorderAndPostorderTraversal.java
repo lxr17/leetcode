fb7453cac40dad54d1320170569fec671f4ce622
何金华 <lanxingren17@gmail.com>
2021-08-18 19:39:05 +0800

    完成第106题

--- src/main/java/com/lxr/leetcode/ConstructBinaryTreeFromInorderAndPostorderTraversal.java
+++ src/main/java/com/lxr/leetcode/ConstructBinaryTreeFromInorderAndPostorderTraversal.java
@@ -1,5 +1,8 @@
 package com.lxr.leetcode;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * Given two integer arrays inorder and postorder where inorder is the inorder traversal of a binary tree and postorder is the postorder traversal of the same tree, construct and return the binary tree.
  * <p>
@@ -22,6 +25,10 @@ package com.lxr.leetcode;
  */
 public class ConstructBinaryTreeFromInorderAndPostorderTraversal {
 
+    /**
+     * 15ms    5.08% Run time
+     * 39.8MB  17.60% Memory
+     */
     public TreeNode buildTree(int[] inorder, int[] postorder) {
         // 思路：迭代
         // 1. 找到根节点（根节点为 postorder 的最后一个节点）
@@ -29,8 +36,100 @@ public class ConstructBinaryTreeFromInorderAndPostorderTraversal {
         // 3. 分别找到左右子树的根节点
         // 4. 重复步骤 2. 和 3.
 
-        // TODO
-        Queue<>
+        // 步骤1.
+        TreeNode root = new TreeNode(postorder[postorder.length - 1]);
+
+        TreeHelper helper = new TreeHelper();
+        helper.treeNode = root;
+        helper.inorder = inorder;
+        helper.postorder = postorder;
+
+        Queue<TreeHelper> queue = new LinkedList<>();
+        queue.add(helper);
+
+        // 步骤2,3,4
+        while (true) {
+            if (queue.isEmpty()) {
+                break;
+            }
+
+            TreeHelper tempHelper = queue.poll();
+            int splitIndex = -1;
+
+            // 找出分割点
+            for (int i = 0; i < tempHelper.inorder.length; i++) {
+                if (tempHelper.inorder[i] == tempHelper.treeNode.val) {
+                    splitIndex = i;
+                    break;
+                }
+            }
+
+            // 拆分
+            int leftSize = splitIndex;
+            int rightSize = tempHelper.inorder.length - splitIndex - 1;
+            int[] leftInorder = new int[leftSize];
+            int[] rightInorder = new int[rightSize];
+            int[] leftPostorder = new int[leftSize];
+            int[] rightPostorder = new int[rightSize];
+            for (int i = 0; i < tempHelper.inorder.length; i++) {
+                if (i < splitIndex) {
+                    leftInorder[i] = tempHelper.inorder[i];
+                } else if (i > splitIndex) {
+                    rightInorder[i - splitIndex - 1] = tempHelper.inorder[i];
+                }
+
+                if (i < splitIndex) {
+                    leftPostorder[i] = tempHelper.postorder[i];
+                } else if (i < tempHelper.postorder.length - 1) {
+                    rightPostorder[i - splitIndex] = tempHelper.postorder[i];
+                }
+            }
+            if (leftSize > 0) {
+                TreeNode leftTree = new TreeNode(leftPostorder[leftSize - 1]);
+                tempHelper.treeNode.left = leftTree;
+
+                if (leftSize > 1) {
+                    TreeHelper leftHelper = new TreeHelper();
+                    leftHelper.treeNode = leftTree;
+                    leftHelper.inorder = leftInorder;
+                    leftHelper.postorder = leftPostorder;
+
+                    queue.add(leftHelper);
+                }
+            }
+            if (rightSize > 0) {
+                TreeNode rightTree = new TreeNode(rightPostorder[rightSize - 1]);
+                tempHelper.treeNode.right = rightTree;
+
+                if (rightSize > 1) {
+                    TreeHelper rightHelper = new TreeHelper();
+                    rightHelper.treeNode = rightTree;
+                    rightHelper.inorder = rightInorder;
+                    rightHelper.postorder = rightPostorder;
+
+                    queue.add(rightHelper);
+                }
+            }
+        }
+
+        return root;
+    }
+
+    public static class TreeHelper {
+        /**
+         * 当前树
+         */
+        private TreeNode treeNode;
+
+        /**
+         * 当前树的中序遍历
+         */
+        private int[] inorder;
+
+        /**
+         * 当前树的后序遍历
+         */
+        private int[] postorder;
     }
 
     public static class TreeNode {
---
+++ src/test/java/com/lxr/leetcode/ConstructBinaryTreeFromInorderAndPostorderTraversalTest.java
@@ -0,0 +1,11 @@
+package com.lxr.leetcode;
+
+import org.junit.Test;
+
+public class ConstructBinaryTreeFromInorderAndPostorderTraversalTest {
+
+    @Test
+    public void buildTree() {
+        new ConstructBinaryTreeFromInorderAndPostorderTraversal().buildTree(new int[]{-1}, new int[]{-1});
+    }
+}
